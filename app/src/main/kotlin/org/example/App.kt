ee7033b2fc38951cca5f4a844c645ccc12b8b5fd
carelli91 <g.carelli@alumno.etec.um.edu.ar>
2025-05-06 08:43:48 -0300

    hola

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,15 @@
+
+package org.example
+
+fun main() {
+    val a = 1
+    val b = 2
+    val c = 3
+
+    if (a == 0) {
+        println("La variable (a) no pueden ser cero")
+    }
+    else {
+        println("La funcion cuadratica es: ${a}x^2 + ${b}x + ${c}")   
+    }
+}
